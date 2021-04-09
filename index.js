76893d0dab1c21275dfedf4a613a1f70c30558e5
root <root@LT-HQ-195.mcc.sg>
2021-04-09 16:37:33 +0800

    editable table

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
-//define data array
+//define data
 var tabledata = [
-    {id:1, name:"Oli Bob", progress:12, gender:"male", rating:1, col:"red", dob:"19/02/1984", car:1},
-    {id:2, name:"Mary May", progress:1, gender:"female", rating:2, col:"blue", dob:"14/05/1982", car:true},
-    {id:3, name:"Christine Lobowski", progress:42, gender:"female", rating:0, col:"green", dob:"22/05/1982", car:"true"},
-    {id:4, name:"Brendon Philips", progress:100, gender:"male", rating:1, col:"orange", dob:"01/08/1980"},
-    {id:5, name:"Margret Marmajuke", progress:16, gender:"female", rating:5, col:"yellow", dob:"31/01/1999"},
-    {id:6, name:"Frank Harbours", progress:38, gender:"male", rating:4, col:"red", dob:"12/05/1966", car:1},
+    {id:1, name:"Oli Bob", progress:12, gender:"male", rating:1, col:"red" },
+    {id:2, name:"Mary May", progress:1, gender:"female", rating:2, col:"blue" },
+    {id:3, name:"Christine Lobowski", progress:42, gender:"female", rating:0, col:"green" },
+    {id:4, name:"Brendon Philips", progress:100, gender:"male", rating:1, col:"orange" },
+    {id:5, name:"Margret Marmajuke", progress:16, gender:"female", rating:5, col:"yellow"},
 ];
 
-//initialize table
-//create Tabulator on DOM element with id "example-table"
+//Build Tabulator
 var table = new Tabulator("#example-table", {
-    data:tabledata,           //load row data from array
-    layout:"fitColumns",      //fit columns to width of table
-    responsiveLayout:"hide",  //hide columns that dont fit on the table
-    tooltips:true,            //show tool tips on cells
-    addRowPos:"top",          //when adding a new row, add it to the top of the table
-    history:true,             //allow undo and redo actions on the table
-    pagination:"local",       //paginate the data
-    paginationSize:7,         //allow 7 rows per page of data
-    movableColumns:true,      //allow column order to be changed
-    resizableRows:true,       //allow row order to be changed
-    initialSort:[             //set the initial sort order of the data
-        {column:"name", dir:"asc"},
+    height:"311px",
+    layout:"fitColumns",
+    reactiveData:true, //turn on data reactivity
+    data:tabledata, //load data into table
+    columns:[
+        {title:"Name", field:"name", sorter:"string", width:200},
+        {title:"Progress", field:"progress", sorter:"number", formatter:"progress"},
+        {title:"Gender", field:"gender", sorter:"string"},
+        {title:"Rating", field:"rating", formatter:"star", hozAlign:"center", width:100},
+        {title:"Favourite Color", field:"col", sorter:"string"},
     ],
-    columns:[                 //define the table columns
-        {title:"Name", field:"name", editor:"input"},
-        {title:"Task Progress", field:"progress", hozAlign:"left", formatter:"progress", editor:true},
-        {title:"Gender", field:"gender", width:95, editor:"select", editorParams:{values:["male", "female"]}},
-        {title:"Rating", field:"rating", formatter:"star", hozAlign:"center", width:100, editor:true},
-        {title:"Color", field:"col", width:130, editor:"input"},
-        {title:"Date Of Birth", field:"dob", width:130, sorter:"date", hozAlign:"center"},
-        {title:"Driver", field:"car", width:90,  hozAlign:"center", formatter:"tickCross", sorter:"boolean", editor:true},
-    ],
+});
+
+//add row to bottom of table on button click
+document.getElementById("reactivity-add").addEventListener("click", function(){
+    tabledata.push({name:"IM A NEW ROW", progress:100, gender:"male"});
+});
+
+//remove bottom row from table on button click
+document.getElementById("reactivity-delete").addEventListener("click", function(){
+    tabledata.pop();
+});
+
+//update name on first row in table on button click
+document.getElementById("reactivity-update").addEventListener("click", function(){
+    tabledata[0].name = "IVE BEEN UPDATED";
 });
